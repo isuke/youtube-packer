3f785fdc1eed0f9740a451452cca212d79506927
Isuke Fujiyama <isuke770@gmail.com>
2014-05-05 15:35:09 +0900

    show url and title in main

--- app/yper.rb
+++ app/yper.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'nokogiri'
+require 'open-uri'
 
 class YPer < Sinatra::Base
   set :public_folder, File.expand_path(File.join(root, '..', 'public'))
@@ -13,6 +15,9 @@ class YPer < Sinatra::Base
   end
 
   post '/main' do
+    @url = params[:url]
+    doc = Nokogiri::HTML(open(@url))
+    @title = doc.title
     erb :main
   end
 end
--- spec/yper_spec.rb
+++ spec/yper_spec.rb
@@ -15,10 +15,21 @@ describe 'The Youtube Packer' do
   end
 
   context "when post '/main'" do
+    before do
+      stub_request(:any, 'http://www.example.com').
+        to_return(body: <<-"EOM", status: 200)
+          <html>
+            <title>Example title</title>
+            <body>Example body</body>
+          </html>
+        EOM
+    end
+
     it "show main" do
-      post '/main'
+      post '/main', url: 'http://www.example.com'
       expect(last_response).to be_ok
-      expect(last_response.body).to include('Youtube Packer')
+      expect(last_response.body).to include('<h2>Example title</h2>')
+      expect(last_response.body).to include('<p>http://www.example.com</p>')
     end
   end
 end
