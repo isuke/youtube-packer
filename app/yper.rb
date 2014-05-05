bea7bcf35133ccd125818acc3bb376c70705dfe6
Isuke Fujiyama <isuke770@gmail.com>
2014-05-05 11:39:44 +0900

    rename 'App' class to 'YPer' class

--- app/yper.rb
+++ app/yper.rb
@@ -1,7 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
-class App < Sinatra::Base
+class YPer < Sinatra::Base
   set :public_folder, File.expand_path(File.join(root, '..', 'public'))
 
   configure :development do
