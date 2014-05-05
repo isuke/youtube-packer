ceab7b18fa1650bfbc922c42b77efdc50d9ab6d9
Isuke Fujiyama <isuke770@gmail.com>
2014-05-05 13:45:33 +0900

    test show main

--- spec/yper_spec.rb
+++ spec/yper_spec.rb
@@ -13,4 +13,12 @@ describe 'The Youtube Packer' do
       expect(last_response.body).to include('Youtube Packer')
     end
   end
+
+  context "when post '/main'" do
+    it "show main" do
+      post '/main'
+      expect(last_response).to be_ok
+      expect(last_response.body).to include('Youtube Packer')
+    end
+  end
 end
