f5a68092ba31c5212c9d54c32b2e0f0891f9760a
Isuke Fujiyama <isuke770@gmail.com>
2014-05-11 17:01:29 +0900

    add bad url error catch

--- app/yper.rb
+++ app/yper.rb
@@ -16,21 +16,31 @@ class YPer < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  class NotURLError < StandardError; end
+
   get '/' do
     erb :index
   end
 
   post '/main' do
-    @url = params[:url]
-    doc = Nokogiri::HTML(open(@url))
-    @title = doc.title
-    @youtube_ids = youtube_ids doc
-    erb :main
+    begin
+      @url = params[:url]
+      raise NotURLError unless @url.url?
+      doc = Nokogiri::HTML(open(@url))
+      @title = doc.title
+      @youtube_ids = youtube_ids doc
+      erb :main
+    rescue OpenURI::HTTPError, SocketError
+      @error_message = 'This value is incorrect url.'
+      raise
+    rescue NotURLError
+      @error_message = 'This value is not url.'
+      raise
+    end
   end
 
   error do
-    @url = params[:url]
-    @error_message = env['sinatra.error'].message
+    @error_message ||= 'Raised unknown error.'
     erb :index
   end
 
